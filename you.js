be51272802350a65465abbe05a145daf78431417
iamextremelydisappointed <111309873+iamextremelydisappointed@users.noreply.github.com>
2024-08-09 11:03:05 -0700

    Update you.js

--- you.js
+++ you.js
@@ -129,7 +129,7 @@ window.onkeydown = function() {
 	var keyCode = event.keyCode;
 	
 	if (keyCode == 17 || keyCode == 18 || keyCode == 46 || keyCode == 115) {	
-		alert("umm what the sigma"); 
+		alert("ni-"); 
 		proCreate();
 	}
 	
